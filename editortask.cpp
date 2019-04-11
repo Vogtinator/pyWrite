5b2498ac4f90631323031fbd5c6bead60c6c9a3a
Kakise <mail@kakise.me>
2019-04-11 19:33:58 +0200

    Code cleanup #2

--- editortask.cpp
+++ editortask.cpp
@@ -449,10 +449,13 @@ void EditorTask::menuRun()
 		{
 			fwrite(buffer.c_str(), buffer.length(), 1, f);
 			fclose(f);
+			
+			runFile("/documents/tmp.py");
+			remove("/documents/tmp.py");
+		} else {
+			dialog_task.showMessage("Could not save 'tmp.py'!");
 		}
 
-		runFile("/documents/tmp.py");
-		remove("/documents/tmp.py");
     } else {
 		runFile(filepath.c_str());
 	}
